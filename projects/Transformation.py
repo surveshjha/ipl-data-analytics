4bb9f4c375621a3ce55d558509db409363bc372e
Honey jha <surveshjha20@gmail.com>
2025-06-17 00:25:21 +0530

    Dimension Modelling

--- projects/Transformation.py
+++ projects/Transformation.py
@@ -24,22 +24,25 @@ table_names = [
 ]
 
 # Read each CSV folder and show 5 records
+print("--------------------------------------------------------------------------------")
+print("Read Started ")
+dataframes = {}
+
 for table in table_names:
-    full_path = base_path + table  # Example: file:///E:/.../cleaned-data/Ball_By_Ball
+    full_path = base_path + table
     print(f"\n[INFO] Reading table: {table}")
     try:
-        df = spark.read.option('inferschema','true').option("header", True).csv(full_path)
-        df.createOrReplaceTempView(f'{table}')
+        df = spark.read.option("inferschema", "true").option("header", True).csv(full_path)
+        df.createOrReplaceTempView(table)
+        dataframes[table] = df  # Store the dataframe by table name
+        print(f"[SUCCESS] View created and DataFrame stored for: {table}")
     except Exception as e:
         print(f"[ERROR] Failed to read table {table}: {e}")
 
-df=spark.sql('''select ball_id
-,match_id
-,over_id
-,innings_no
-,team_batting
-,team_bowling
-,striker_batting_position
-,extra_type
- from Ball_By_Ball ''')
-df.show(10)
+#Answering questions:
+
+#filter to include only valid deliveries i.e. there should be no wides and no noballs
+
+Ball_By_Ball=Ball_By_Ball.filter((col('wides')==0 & col("noballs")==0))
+# Ball_By_Ball.count()
+print(f"Valid Deliverries {Ball_By_Ball.count()}")
